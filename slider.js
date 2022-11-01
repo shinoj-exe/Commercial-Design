a9946625b743a55f7ea76c8d9dccea264ce931c2
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2022-11-01 22:34:35 +0530

    updated slider

---
+++ slider.js
@@ -0,0 +1,30 @@
+var index = 0;
+var slides = document.querySelectorAll(".slides");
+// var dot = document.querySelectorAll(".dot");
+
+function changeSlide(){
+    console.log("Hekkio");
+
+  if(index<0){
+    index = slides.length-1;
+  }
+  
+  if(index>slides.length-1){
+    index = 0;
+  }
+  
+  for(let i=0;i<slides.length;i++){
+    slides[i].style.display = "none";
+    // dot[i].classList.remove("active");
+  }
+  
+  slides[index].style.display= "block";
+//   dot[index].classList.add("active");
+  
+  index++;
+  
+  setTimeout(changeSlide,2000);
+  
+}
+
+changeSlide();
